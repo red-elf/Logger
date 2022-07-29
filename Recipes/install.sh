e7374abda9cf6e32e65181d5b041f8fce03cfd0c
Milos Vasic <milos85vasic@gmail.com>
2022-07-29 20:07:10 +0300

    Build config updated. Install recipe supports the 'Versionable'.

--- Recipes/install.sh
+++ Recipes/install.sh
@@ -5,7 +5,9 @@ echo "Installing the 'Logger' library, please wait" && \
   rm -rf ./Build && \
   mkdir Build && \
   cd Build && \
-  cmake .. && \
+  source ../../Version/version.sh && cmake -DVERSIONABLE_VERSION_PRIMARY="$VERSIONABLE_VERSION_PRIMARY" \
+  -DVERSIONABLE_VERSION_SECONDARY="$VERSIONABLE_VERSION_SECONDARY" -DVERSIONABLE_NAME="$VERSIONABLE_NAME" \
+  -DVERSIONABLE_VERSION_PATCH="$VERSIONABLE_VERSION_PATCH" .. && \
   make -j "$(nproc)" && \
   sudo make install && \
   echo "The 'Logger' library has been installed with success"
