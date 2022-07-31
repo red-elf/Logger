c2cbf4a0d86d2f3b6452090886eae18e2853109a
Milos Vasic <milos85vasic@gmail.com>
2022-07-31 18:23:21 +0300

    Using ninja to build.

--- Recipes/install.sh
+++ Recipes/install.sh
@@ -5,9 +5,9 @@ echo "Installing the 'Logger' library, please wait" && \
   rm -rf ./Build && \
   mkdir Build && \
   cd Build && \
-  source ../../Version/version.sh && cmake -DVERSIONABLE_VERSION_PRIMARY="$VERSIONABLE_VERSION_PRIMARY" \
+  source ../../Version/version.sh && cmake -GNinja -DVERSIONABLE_VERSION_PRIMARY="$VERSIONABLE_VERSION_PRIMARY" \
   -DVERSIONABLE_VERSION_SECONDARY="$VERSIONABLE_VERSION_SECONDARY" -DVERSIONABLE_NAME="$VERSIONABLE_NAME" \
   -DVERSIONABLE_VERSION_PATCH="$VERSIONABLE_VERSION_PATCH" .. && \
-  make -j "$(nproc)" && \
-  sudo make install && \
+  ninja -j "$(nproc)" && \
+  sudo ninja install && \
   echo "The 'Logger' library has been installed with success"
