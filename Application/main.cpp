4c1f57bfa63134d039649241cd8383f49f688dd1
Milos Vasic <milos85vasic@gmail.com>
2022-08-05 22:19:36 +0300

    Handling the program arguments - First touches.

--- Application/main.cpp
+++ Application/main.cpp
@@ -1,10 +1,24 @@
+#include <string>
+#include <argparse/argparse.hpp>
 #include "Logger.h"
 #include "LogLevel.h"
 #include "LoggerSimple.h"
 #include "LoggerFactory.h"
 #include "LoggerLibrary.h"
+#include "BuildConfig.h"
 
 int main() {
 
+    auto version = std::to_string(VERSIONABLE_VERSION_PRIMARY) +
+                             "." + std::to_string(VERSIONABLE_VERSION_SECONDARY) +
+                             "." + std::to_string(VERSIONABLE_VERSION_PATCH);
+
+    if (VERSIONABLE_SNAPSHOT) {
+
+        version += "-SNAPSHOT";
+    }
+
+    argparse::ArgumentParser program("relog", version);
+    program.add_argument("-v", "--version");
     return 0;
 }
