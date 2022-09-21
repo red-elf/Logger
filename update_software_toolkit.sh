6c50fb4a9222bb010b009165da9320d438678fd1
Milos Vasic <i@mvasic.ru>
2022-09-21 15:04:04 +0300

    The Software Toolkit update script - Update from upstreams.

--- update_software_toolkit.sh
+++ update_software_toolkit.sh
@@ -18,7 +18,17 @@ function UPDATE_MODULE {
 
   if test -e "$DIR"; then
 
-    if cd "$DIR" && git checkout main && git fetch && git pull && cd "$HERE"; then
+    if cd "$DIR" && git checkout main && git fetch && git pull; then
+
+      if test -e "Upstreams"; then
+
+        echo "We are going to update from the upstreams"
+        if ! git fetch upstream && git pull upstream main; then
+
+          echo "ERROR: Failed to fetch and pull the upstreams"
+          exit 1
+        fi
+      fi
 
       cd "$HERE" &&
         echo "The Software Toolkit module updated: '$DIR'"
